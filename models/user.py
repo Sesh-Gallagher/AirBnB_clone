2c369467029041b5d04b2d662b8c2453cdac0935
Sesh-Gallagher <t.ntwana@gmail.com>
2024-05-21 20:13:48 -0700

    hbnb fix

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Represents a class for managing user objects"""
 
+    email = ""
+    password = ""
     first_name = ""
     last_name = ""
-    passsword = ""
-    email = ""
